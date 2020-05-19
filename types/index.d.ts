6a768d19a31d2006d385f593718afa421dbe11a7
Vini Andrade <vini.andrade.dev@gmail.com>
2020-05-19 13:47:09 +0200

    Enable types on <MessageCommerce /> component
    Signed-off-by: Vini Andrade <vini.andrade.dev@gmail.com>

--- types/index.d.ts
+++ types/index.d.ts
@@ -960,9 +960,22 @@ export const ChannelSearch: React.FC<any>;
 export const LoadMorePaginator: React.FC<LoadMorePaginatorProps>;
 export const InfiniteScrollPaginator: React.FC<InfiniteScrollPaginatorProps>;
 export const LoadingIndicator: React.FC<LoadingIndicatorProps>;
+export interface MessageCommerceProps extends MessageUIComponentProps {
+  isMyMessage(message: Client.MessageResponse): boolean;
+  t: i18next.TFunction;
+  tDateTimeParser: (
+    datetime: string | number,
+  ) => { format: (f: string) => string };
+}
+
+export type MessageCommerceState = {
+  isFocused: boolean;
+  showDetailedReactions: boolean;
+};
+
 export class MessageCommerce extends React.PureComponent<
-  MessageUIComponentProps,
-  any
+  MessageCommerceProps,
+  MessageCommerceState
 > {}
 export class MessageLivestream extends React.PureComponent<
   MessageUIComponentProps,
